9f0b1c921a3156a7e3e493618d0b534209124d00
AndyEveritt <andreweveritt@e3d-online.com>
2020-11-03 13:26:27 +0000

    add wrapper methods

--- DuetWebAPI/dwa_factory.py
+++ DuetWebAPI/dwa_factory.py
@@ -48,8 +48,40 @@ class DuetWebAPIFactory:
 
     def create_wrapper(self, creator: DuetAPI):
         class DuetAPIWrapper(creator):
-            def demo(self):
-                print('test')
+            def get_coords(self):
+                model = self.get_model()
+                axes = model['move']['axes']
+                ret = {}
+                for i in range(0, len(axes)):
+                    ret[axes[i]['letter']] = axes[i]['userPosition']
+                return(ret)
+
+            def get_layer(self):
+                model = self.get_model()
+                layer = model['job']['layer']
+                if not layer:
+                    layer = 0
+                return(layer)
+
+            def get_num_extruders(self):
+                model = self.get_model()
+                extruders = model['move']['extruders']
+                return len(extruders)
+
+            def get_num_tools(self):
+                model = self.get_model()
+                tools = model['tools']
+                return len(tools)
+
+            def get_status(self):
+                model = self.get_model()
+                status = model['state']['status']
+                return status
+
+            def get_temperatures(self):
+                model = self.get_model()
+                sensors = model['sensors']['analog']
+                return sensors
 
         return DuetAPIWrapper
 
