40298e03b60acae94ae7e6f23c70400910ac4336
AndyEveritt <andreweveritt@e3d-online.com>
2021-03-03 09:24:58 +0000

    v1.2.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="duetwebapi",
-    version="1.2.1",
+    version="1.2.2",
     include_package_data=True,
     packages=find_packages(),
 
